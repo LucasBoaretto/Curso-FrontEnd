b043dfae76cd833ed285390bdb94d11653e69199
Lucas Luiz Boaretto <lcboaretto@hotmail.com>
2025-03-26 21:16:21 -0300

    update

--- FakeGenerator - trabalho final/script/script.js
+++ FakeGenerator - trabalho final/script/script.js
@@ -1,1 +1,25 @@
-var texto = document.getElementById('input#inhome');
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+ }
+
+function toggleCase() {
+    var txt = document.getElementById('inhome').value; 
+     var mostrar = document.getElementById('txt2'); 
+
+    var resultado = '';
+    for (var i = 0; i < txt.length; i++) {
+        var letra = txt[i];
+        if (letra === letra.toUpperCase()) {
+            resultado += letra.toLowerCase();
+        } else {
+            resultado += letra.toUpperCase(); 
+        }
+    }
+
+    mostrar.value = resultado; 
+}
+
