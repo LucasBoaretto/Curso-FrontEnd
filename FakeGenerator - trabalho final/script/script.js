76f747a17b49213129ca7a50fb7a38dfd95f0fca
Lucas Luiz Boaretto <lcboaretto@hotmail.com>
2025-03-30 19:47:24 -0300

    update organização

--- FakeGenerator - trabalho final/script/script.js
+++ FakeGenerator - trabalho final/script/script.js
@@ -1,3 +1,6 @@
+var text = document.getElementById('inhome');
+var mostrar = document.getElementById('txt2');
+
 function clickMenu() {
     if (menu.style.display == 'block') {
         menu.style.display = 'none';
@@ -6,22 +9,26 @@ function clickMenu() {
     }
 }
 
+const alerta = ()=>{ 
+    alert('ok')
+}
 
-function toggleCase() {
-    var txt = document.getElementById('inhome').value;
-    var mostrar = document.getElementById('txt2');
+alerta();
 
-    var resultado = '';
-    for (var i = 0; i < txt.length; i++) {
-        var letra = txt[i];
-        if (letra === letra.toUpperCase()) {
-            resultado += letra.toLowerCase();
-        } else {
-            resultado += letra.toUpperCase();
-        }
-    }
 
-    mostrar.value = resultado;
+function toggleCase(text, mostrar) {
+    console.log(text);
+    // var resultado = '';
+    // for (var i = 0; i < text.length; i++) {
+    //     var letra = text[i];
+    //     if (letra === letra.toUpperCase()) {
+    //         resultado += letra.toLowerCase();
+    //     } else {
+    //         resultado += letra.toUpperCase();
+    //     }
+    // }
+
+    // mostrar.value = resultado;
 }
 
 function contador() {
